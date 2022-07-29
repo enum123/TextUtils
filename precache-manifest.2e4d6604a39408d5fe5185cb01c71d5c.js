9909e6fdbd4e2f0f0473d3ee65a55aff0bad8ffc
Usama Saif <usamaalvi65@gmail.com>
2022-07-30 00:13:29 +0500

    Updates

---
+++ precache-manifest.2e4d6604a39408d5fe5185cb01c71d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58a91a931a5b5bf405ee",
+    "url": "/TextUtils/static/css/main.b9f10c49.chunk.css"
+  },
+  {
+    "revision": "58a91a931a5b5bf405ee",
+    "url": "/TextUtils/static/js/main.58a91a93.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/TextUtils/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "56e43470182c5aec8fda",
+    "url": "/TextUtils/static/js/runtime~main.56e43470.js"
+  },
+  {
+    "revision": "7ff3fcc35931fbf9c70f0fc859aaed93",
+    "url": "/TextUtils/index.html"
+  }
+];
